e705141b71817b8f0edbe90e73a6e74a398c969a
Alberto <alberto@villelabrasillabs.com>
2025-10-16 17:43:08 -0300

    adicionando código dos testes de manipulação de testo

--- Texto/Program.cs
+++ Texto/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string brinksDaSilva = $"o {{betu}} é \"magro\"";
+
+Console.WriteLine(brinksDaSilva);
