32ee0c8518e5c3440f7a33708be5d5f166064809
Alberto <alberto@villelabrasillabs.com>
2025-10-20 14:16:30 -0300

    estudos de domingo

---
+++ Arquivo/Classes/LeitorDeArquivos.cs
@@ -0,0 +1,28 @@
+﻿namespace Arquivo.Classes;
+
+public static class LeitorDeArquivos
+{
+
+
+    // Devoluções:
+    // 0 número total de bytes lidos do buffer. Isso poderá ser menor que o número de
+    // bytes solicitado se esse número de bytes não estiver disponível no momento, ou
+    //zero, se o final do fluxo for atingido
+    public static void Read(byte[] arrayBuffer, int offset, int count)
+    {
+        // Implementação fictícia para ilustrar o método:
+
+        //ArrayBuffer: Buffer onde os dados serão armazenados,
+        //Offset: Posição inicial,
+        //Count: Quantidade de bytes a serem lidos
+    }
+
+    public static void EscreverBuffer(byte[] buffer)
+    {
+        foreach (byte meuByte in buffer)
+        {
+            Console.Write(System.Text.Encoding.UTF8.GetString(buffer));
+            Console.Write(" ");
+        }
+    }
+}
--- Arquivo/Program.cs
+++ Arquivo/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Arquivo.Classes;
+
+string enderecoDoArquivo = "contas.txt";
+int numeroDeBytesLidos = -1;
+FileStream fluxoDoArquivo = new(enderecoDoArquivo, FileMode.Open);
+
+//1024 bytes = 1 KB
+byte[] buffer = new byte[1024];
+
+while(numeroDeBytesLidos != 0)
+{
+    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+    LeitorDeArquivos.EscreverBuffer(buffer);
+}
+
+
+
+
+
+
