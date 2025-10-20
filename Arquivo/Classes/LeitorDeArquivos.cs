341eb62bde59f3590a27b9ec8316026cc4e1ee0b
Alberto <alberto@villelabrasillabs.com>
2025-10-20 14:52:52 -0300

    ajustando comentário

--- Arquivo/Classes/LeitorDeArquivos.cs
+++ Arquivo/Classes/LeitorDeArquivos.cs
@@ -4,10 +4,10 @@ public static class LeitorDeArquivos
 {
 
 
-    // Devoluções:
-    // 0 número total de bytes lidos do buffer. Isso poderá ser menor que o número de
-    // bytes solicitado se esse número de bytes não estiver disponível no momento, ou
-    //zero, se o final do fluxo for atingido
+    /*Devoluções:
+     0 número total de bytes lidos do buffer. Isso poderá ser menor que o número de
+    bytes solicitado se esse número de bytes não estiver disponível no momento, ou
+    zero, se o final do fluxo for atingido*/
     public static void Read(byte[] arrayBuffer, int offset, int count)
     {
         // Implementação fictícia para ilustrar o método:
